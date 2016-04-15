bb84f5478e9039033b91c93423a203577535e976
Marek Lewiński <lewinski.marek@outlook.com>
2016-04-15 13:07:40 +0200

    Created Transformers.cs

---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    delegate long Transformer(long x);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Transformers transformers = new Transformers();
+            long[,] table = new long[10,10];
+            Random random = new Random();
+            for (int i = 0; i < 10; i++)
+                for (int j = 0; j < 10; j++) table[i,j] = (random.Next()%100);
+            PrintTable(table);
+            Transformer t = transformers.OddOrEven;
+            Transform(table, t);
+            PrintTable(table);
+            Console.ReadLine();
+        }
+
+        static void PrintTable(long[,] table)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++) Console.Write(" {0}", table[i, j]);
+                Console.WriteLine();
+            }
+        }
+        static void Transform(long[,] table, Transformer Transformer)
+        {
+            for (int i = 0; i < 10; i++)
+                for (int j = 0; j < 10; j++)
+                    table[i, j] = Transformer(table[i, j]);
+        }
+    }
+}
---
+++ Delegates/Delegates/Transformers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public class Transformers
+    {
+        public long Square(long x) => x*x;
+        public long MultiplyByTwo(long x) => x*2;
+        public long OddOrEven(long x) => (long) x%2;
+    }
+}
