d8de6eefe91fca331b13410a6e4e8d53fff239a6
Marek Lewiński <lewinski.marek@outlook.com>
2016-04-15 12:35:12 +0200

    Stack implemented

---
+++ GenericDataStructures/GenericDataStructures/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            Console.WriteLine(stack.Empty());
+            for(int i = 0; i<10; i++) stack.Push(i);
+            for(int i = 0; i<10; i++) Console.WriteLine(stack.Pop());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GenericDataStructures/GenericDataStructures/Stack.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDataStructures
+{
+    public class Stack<T>
+    {
+        private List<T> StackArray = new List<T>();
+
+        public void Push(T element)
+        {
+            StackArray.Add(element);
+        }
+
+        public T Pop()
+        {
+            T tmp = StackArray[StackArray.Count-1];
+            StackArray.RemoveAt(StackArray.Count-1);
+            return tmp;
+        }
+
+        public bool Empty()
+        {
+            return (StackArray.Count == 0);
+        }
+
+        public int Size => StackArray.Count;
+    }
+}
