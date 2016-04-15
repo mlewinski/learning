b11d02476b00db777fa199125ad13dc4e19fea40
Marek Lewiński <lewinski.marek@outlook.com>
2016-04-15 13:22:31 +0200

    Created project "Events"

---
+++ Delegates/Events/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stock st = new Stock("Koza");
+            st.PriceChanged += (new Program()).PriceChangedHandle;
+            st.Price = 10;
+            st.Price = 15;
+            Console.ReadLine();
+        }
+
+        public void PriceChangedHandle(decimal oldPrice, decimal price)
+        {
+            Console.WriteLine("Price changed from {0} to {1} !", oldPrice, price);
+        }
+    }
+}
---
+++ Delegates/Events/Stock.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Events
+{
+    public delegate void PriceChangedHandler(decimal oldPrice, decimal newPrice);
+    class Stock
+    {
+        private string symbol;
+        private decimal price;
+
+        public Stock(string symbol)
+        {
+            this.symbol = symbol;
+        }
+
+        public event PriceChangedHandler PriceChanged;
+
+        public decimal Price
+        {
+            get { return price; }
+            set
+            {
+                if (price == value) return;
+                decimal oldPrice = price;
+                price = value;
+                PriceChanged?.Invoke(oldPrice, price);
+            }
+        }
+    }
+}
