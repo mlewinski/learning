c1c7c021fae38352551e761df89947e678c2587a
Marek Lewiński <lewinski.marek@outlook.com>
2016-04-15 12:47:59 +0200

    Queue Implementation

--- GenericDataStructures/GenericDataStructures/Program.cs
+++ GenericDataStructures/GenericDataStructures/Program.cs
@@ -15,6 +15,12 @@ namespace GenericDataStructures
             for(int i = 0; i<10; i++) stack.Push(i);
             for(int i = 0; i<10; i++) Console.WriteLine(stack.Pop());
             Console.ReadLine();
+
+            Queue<int> queue = new Queue<int>();
+            Console.WriteLine(queue.Empty());
+            for (int i = 0; i < 10; i++) queue.Push(i);
+            for (int i = 0; i < 10; i++) Console.WriteLine(queue.Pop());
+            Console.ReadLine();
         }
     }
 }
--- GenericDataStructures/GenericDataStructures/Queue.cs
+++ GenericDataStructures/GenericDataStructures/Queue.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace GenericDataStructures
 {
-    class Queue
+    class Queue<T>
     {
+        private List<T> QueueArray = new List<T>();
+
+        public void Push(T element)
+        {
+            QueueArray.Add(element);
+        }
+
+        public T Pop()
+        {
+            T tmp = QueueArray[0];
+            QueueArray.RemoveAt(0);
+            //for (int i = 1; i < QueueArray.Count; i++) QueueArray[i - 1] = QueueArray[i];
+            return tmp;
+        }
+
+        public bool Empty()
+        {
+            return (QueueArray.Count == 0);
+        }
+
+        public int Size => QueueArray.Count;
     }
 }
